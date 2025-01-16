afbc2d2904661895658f37f6c8d520d777a5afdc
savelii <savelkayamil5@gmail.com>
2025-01-16 19:25:00 +0300

    add build asm

--- main.go
+++ main.go
@@ -5,3 +5,9 @@ import "fmt"
 func main() {
 	fmt.Println("Hello World")
 }
+
+// go tool objdump -s "main.main" ./main > output.asm
+
+// GOARCH=amd64 GOOS=windows go build main.go
+// GOARCH=arm64 GOOS=darwin go build main.go
+// GOARCH=arm64 GOOS=linux go build main.go
